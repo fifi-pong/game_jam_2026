1a5a0224d4b860df20b85121e060fde86f07ee0c
fridafuglerud@gamil.com <fridafuglerud@gmail.com>
2026-03-17 14:03:01 +0100

    ny_base

--- FlippJack_main.py
+++ FlippJack_main.py
@@ -1,11 +1,40 @@
-"""import pygame, random
+import pygame, random
 
 # 1. Oppsett
 class FlippJack:
-    pygame.init()
-    self.skjerm = pygame.display.set_mode((800, 600))
-    kjorer = True
+    def __init__(self):
+        pygame.init()
+        self.skjerm = pygame.display.set_mode((800, 600))
+        self.clock = pygame.time.Clock()
+        self.running = True
 
+class Objecter:
+    def __init__(self):
+        pass
+
+class Spiller(Objecter):
+    def __init__(self):
+        super().__init__()
+        pass
+
+class Laser(Objecter):
+    def __init__(self):
+        super().__init__()
+        pass
+
+class Kanon(Objecter):
+    def __init__(self):
+        super().__init__()
+        pass
+
+class Vegger(Objecter):
+    def __init__(self):
+        super().__init__()
+        pass
+
+
+
+"""
 # 2. Spill-løkken (Game Loop)
 while kjorer:
     for hendelse in pygame.event.get():
@@ -15,4 +44,5 @@ while kjorer:
     skjerm.fill((0, 0, 0)) # Gjør skjermen svart
     pygame.display.flip()  # Oppdater skjermen
 
-pygame.quit()"""
+pygame.quit()
+"""
