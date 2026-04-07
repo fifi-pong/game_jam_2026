5d2f202c32256a4993b571858ba75af4c7277953
MrBeast2026 <fredkick2@gmail.com>
2026-04-07 12:23:43 +0200

    Legget_til_filter_oppå_bilde

--- state_management/states/menu_state.py
+++ state_management/states/menu_state.py
@@ -21,6 +21,11 @@ class MenuState(BaseState):
             # Fallback hvis filen mangler
             self.bakgrunn = pygame.Surface((BREDDE, HOYDE))
             self.bakgrunn.fill(MORK_BLA)
+
+            # Leger et mørkt filter oppå bilde
+            self.mork_film = pygame.Surface((800, 600))
+            self.mork_film.fill ((0, 0, 0))
+            self.mork_film.set_alpha(180) # Jo høyere tall, jo mørkere (0-255)
         
 
     def handle_events(self, events : list[pygame.event.Event]):
@@ -40,4 +45,4 @@ class MenuState(BaseState):
     def draw(self, surface: pygame.Surface):
         # 1. Tegn bakgrunnen først
         skjerm.blit(self.bakgrunn, (0, 0))
-        self.draw_text(surface, "Du er i hovedmenyen! Trykk SPACE for å starte.", self.font, (255, 255, 255), (250, 250))
+        self.draw_text(surface, "Du er i hovedmenyen! Trykk SPACE for å starte.", self.font, (255, 255, 255,), (250, 250))
